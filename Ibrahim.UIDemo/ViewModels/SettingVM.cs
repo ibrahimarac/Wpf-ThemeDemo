41ca72a5046da7d5349bbb11c4ea0177309eccf6
Halil İbrahim ARAÇ <iboarac@gmail.com>
2021-06-19 00:21:38 +0300

    ComboBox stili eklendi

--- Ibrahim.UIDemo/ViewModels/SettingVM.cs
+++ Ibrahim.UIDemo/ViewModels/SettingVM.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Ibrahim.UI.Views;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,29 @@ namespace Ibrahim.UI.ViewModels
 {
     class SettingVM
     {
+        string selectedTheme;
+
+        public IEnumerable<string> Themes { get; set; }
+
+        public string SelectedTheme
+        {
+            get
+            {
+                return selectedTheme;
+            }
+            set
+            {
+                selectedTheme = value;
+                OnPropertyChanged("SelectedTheme");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
 
     }
 }
